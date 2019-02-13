49f65c4b2fb26860744c9943596dd56003fca0e0
Rachel Williams <williamsrachelada@gmail.com>
2019-02-13 04:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === 'hello') {
+  if (string === string.toLowerCase()) {
     return 'I can\'t hear you!';
-  } else if (string === 'HELLO') {
+  } else if (string === string.toUpperCase()) {
     return 'YES INDEED!';
   } else {
     return 'I love you, too.'
